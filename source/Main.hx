ba3ecf77cfe47fd9599a9145ee30e9f04ad81895
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-09 14:59:05 +0200

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -86,9 +86,6 @@ class Main extends Sprite
 
 	private function setupGame():Void
 	{
-	        #if mobile
-		Generic.mode = INTERNAL;
-		#end
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
