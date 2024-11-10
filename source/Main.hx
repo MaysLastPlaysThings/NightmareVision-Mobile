d62605227884ccf15854ec0534372c9077185d2c
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-10 20:32:30 +0200

    I FORGOR

--- source/Main.hx
+++ source/Main.hx
@@ -86,6 +86,9 @@ class Main extends Sprite
 
 	private function setupGame():Void
 	{
+	  #if mobile
+	  SUtil.doTheCheck();
+	  #end
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
