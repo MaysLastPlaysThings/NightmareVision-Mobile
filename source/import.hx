615f248859aa829608ea8f461685733d57806c5a
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-11 14:23:49 +0200

    update source/import.hx

--- source/import.hx
+++ source/import.hx
@@ -22,6 +22,26 @@ import Init;
 import mobile.utils.StorageUtil;
 #end 
 
+// Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
+#if sys
+import sys.*;
+import sys.io.*;
+#elseif js
+import js.html.*;
+#end
+
 import hxvlc.flixel.*;
 
 import meta.data.Paths;
