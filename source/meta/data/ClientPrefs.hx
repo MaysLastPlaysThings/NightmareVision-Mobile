2cee914b052f1fb0d6870d87291855a2839372f7
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-12 07:31:57 +0200

    update source/meta/data/ClientPrefs.hx

--- source/meta/data/ClientPrefs.hx
+++ source/meta/data/ClientPrefs.hx
@@ -26,7 +26,7 @@ class ClientPrefs {
 	public static var loadingThreads:Int = Math.floor(Std.parseInt(Sys.getEnv("NUMBER_OF_PROCESSORS"))/2);
 	public static var multicoreLoading:Bool = false;
 	#if android
-	public static var storageType:String = "EXTERNAL_DATA";
+	public static var storageType:String = "EXTERNAL";
 	#end
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
