bcefd1754bf796576cb321738495d7abf5a76258
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-20 13:15:18 +0200

    Fuck

--- source/mobile/substates/MobileOptionsSubState.hx
+++ source/mobile/substates/MobileOptionsSubState.hx
@@ -26,7 +26,7 @@ import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import meta.data.Controls;
-import meta.data.options.BaseOptionsMenu;
+import meta.data.options.*;
 import meta.data.*;
 import meta.states.*;
 import gameObjects.*;
