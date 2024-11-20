c4f6c3e5699ac0c5f04a7d486d3dd24425797f6e
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-11-20 12:42:55 +0200

    lmao

--- source/mobile/substates/MobileOptionsSubState.hx
+++ source/mobile/substates/MobileOptionsSubState.hx
@@ -3,6 +3,7 @@ package mobile.substates;
 #if desktop
 import meta.data.Discord.DiscordClient;
 #end
+
 import openfl.text.TextField;
 import flixel.FlxCamera;
 import flixel.FlxG;
@@ -34,11 +35,25 @@ using StringTools;
 
 class MobileOptionsSubState extends BaseOptionsMenu
 {
+	#if android
+	var storageTypes:Array<String> = ["EXTERNAL_DATA", "EXTERNAL_OBB", "EXTERNAL_MEDIA", "EXTERNAL"];
+	var externalPaths:Array<String> = StorageUtil.checkExternalPaths(true);
+	final lastStorageType:String = ClientPrefs.data.storageType;
+	#end
+	var option:Option;
 
 	public function new()
 	{
-		title = 'Mobile Settings';
-		rpcTitle = 'Mobile Settings Menu'; //for Discord Rich Presence
+		#if android if (!externalPaths.contains('\n'))
+			storageTypes = storageTypes.concat(externalPaths); #end
+		title = 'Mobile Options';
+		rpcTitle = 'Mobile Options Menu'; // for Discord Rich Presence
+
+		#if android
+		option = new Option('Storage Type', 'Which folder Psych Engine should use?\n(CHANGING THIS DELETES YOUR OLD FOLDER!!)', 'storageType', STRING,
+			storageTypes);
+		addOption(option);
+		#end
 
 		option = new Option('Controls Type', 
 		 'What controls do you want to use?',
@@ -49,5 +64,34 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		addOption(option);
 
 		super();
+	}
+
+	#if android
+	function onStorageChange():Void
+	{
+		File.saveContent(lime.system.System.applicationStorageDirectory + 'storagetype.txt', ClientPrefs.data.storageType);
+
+		var lastStoragePath:String = StorageType.fromStrForce(lastStorageType) + '/';
+
+		try
+		{
+			Sys.command('rm', ['-rf', lastStoragePath]);
+		}
+		catch (e:haxe.Exception)
+			trace('Failed to remove last directory. (${e.message})');
+	}
+	#end
+
+	override public function destroy()
+	{
+		super.destroy();
+		#if android
+		if (ClientPrefs.data.storageType != lastStorageType)
+		{
+			onStorageChange();
+			CoolUtil.showPopUp('Storage Type has been changed and you need to restart the game!!\nPress OK to close the game.', 'Notice!');
+			lime.system.System.exit(0);
+		}
+		#end
 	}
 }
