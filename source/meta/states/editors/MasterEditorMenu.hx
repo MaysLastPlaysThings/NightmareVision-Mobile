3468a3564676a4e371a6b12ba19adfed51b51811
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-12-02 13:40:29 +0200

    update source/meta/states/editors/MasterEditorMenu.hx

--- source/meta/states/editors/MasterEditorMenu.hx
+++ source/meta/states/editors/MasterEditorMenu.hx
@@ -83,6 +83,9 @@ class MasterEditorMenu extends MusicBeatState
 		changeSelection();
 
 		FlxG.mouse.visible = false;
+		#if mobile
+		addVirtualPad(LEFT_FULL, A_B);
+		#end
 		super.create();
 	}
 
