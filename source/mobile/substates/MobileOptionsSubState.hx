0b0d3e3fbc2a214e5b8a9349f4b586031cb4ea3f
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-12-05 14:21:20 +0200

    yea

--- source/mobile/substates/MobileOptionsSubState.hx
+++ source/mobile/substates/MobileOptionsSubState.hx
@@ -36,25 +36,13 @@ using StringTools;
 
 class MobileOptionsSubState extends BaseOptionsMenu
 {
-	#if android
-	var storageTypes:Array<String> = ["EXTERNAL", "NO_STORAGE"];
-	var externalPaths:Array<String> = StorageUtil.checkExternalPaths(true);
-	final lastStorageType:String = ClientPrefs.storageType;
-	#end
 	var option:Option;
 
 	public function new()
 	{
-		#if android if (!externalPaths.contains('\n'))
-			storageTypes = storageTypes.concat(externalPaths); #end
+
 		title = 'Mobile Settings';
 		rpcTitle = 'Mobile Settings Menu'; // for Discord Rich Presence
-
-		#if android
-		option = new Option('Storage Type', 'Which folder Psych Engine should use?\n(CHANGING THIS DELETES YOUR OLD FOLDER!!)', 'storageType', 'string', 'EXTERNAL_DATA',
-			storageTypes);
-		addOption(option);
-		#end
 
 		var option:Option = new Option('V-Pad Opacity', // mariomaster was here again
 			'Changes V-Pad Opacity -yeah ', 'padalpha', 'float', 0.5);
@@ -83,35 +71,5 @@ class MobileOptionsSubState extends BaseOptionsMenu
 		addOption(option);*/
 
 		super();
-	}
-
-	#if android
-	function onStorageChange():Void
-	{
-		File.saveContent(lime.system.System.applicationStorageDirectory + 'storagetype.txt', ClientPrefs.storageType);
-
-		var lastStoragePath:String = StorageType.fromStrForce(lastStorageType) + '/';
-
-		try
-		{
-			Sys.command('rm', ['-rf', lastStoragePath]);
-		}
-		catch (e:haxe.Exception)
-			trace('Failed to remove last directory. (${e.message})');
-	}
-	#end
-
-	override public function destroy()
-	{
-		super.destroy();
-		#if android
-		if (ClientPrefs.storageType != lastStorageType)
-		{
-			onStorageChange();
-			CoolUtil.showPopUp('Storage Type has been changed and you need to restart the game!!\nPress OK to close the game.', 'Notice!');
-			ClientPrefs.saveSettings();
-			lime.system.System.exit(0);
-		}
-		#end
 	}
 }
